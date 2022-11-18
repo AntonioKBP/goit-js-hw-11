531e8992d6ea1e8a3cf1c9fc03d24a6500c62e01
Anton Kovsh <kovsh_anton@ukr.net>
2022-11-18 15:48:37 +0200

    upd

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,22 @@
 const inputRef = document.querySelector('.search-form');
 const searchBtnRef = document.querySelector('button[type="submit"]');
-console.log(inputRef);
-console.log(searchBtnRef);
 
 // https://pixabay.com/api/?key=31349139-c34332f5cc1455d1f889740ec&q=yellow+flowers&image_type=photo
+
+function getData() {
+  return fetch(
+    'https://pixabay.com/api/?key=31349139-c34332f5cc1455d1f889740ec&q=yellow+flowers&image_type=photo'
+  )
+    .then(r => r.json())
+    .then(data => console.log(data));
+}
+getData();
+
+inputRef.addEventListener('input', onInputChange);
+searchBtnRef.addEventListener('click', toMarkup);
+
+function onInputChange(e) {
+  console.log(e.target.value);
+}
+
+function toMarkup() {}
