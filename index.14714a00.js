12d71148145b819473659b4d40636ea97ae4b160
AntonioKBP <105855258+AntonioKBP@users.noreply.github.com>
2022-11-18 13:07:06 +0000

    Deploying to gh-pages from @ AntonioKBP/goit-js-hw-11@f8937a383b6148db0da77179b712e05047c2da65 🚀

--- index.14714a00.js
+++
@@ -1,2 +0,0 @@
-console.log("hello 11");
-//# sourceMappingURL=index.14714a00.js.map
---
+++ index.99634110.js
@@ -0,0 +1,2 @@
+var inputRef=document.querySelector(".search-form"),searchBtnRef=document.querySelector('button[type="submit"]');console.log(inputRef),console.log(searchBtnRef);
+//# sourceMappingURL=index.99634110.js.map
