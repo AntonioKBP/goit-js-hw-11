f36f32f1b4ee97f0f51918a680a03612dcb40e62
Anton Kovsh <kovsh_anton@ukr.net>
2022-11-18 18:35:13 +0200

    upd

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,6 @@
 const inputRef = document.querySelector('.search-form');
 const searchBtnRef = document.querySelector('button[type="submit"]');
+import debounce from 'lodash.debounce';
 
 // https://pixabay.com/api/?key=31349139-c34332f5cc1455d1f889740ec&q=yellow+flowers&image_type=photo
 
@@ -8,15 +9,70 @@ function getData() {
     'https://pixabay.com/api/?key=31349139-c34332f5cc1455d1f889740ec&q=yellow+flowers&image_type=photo'
   )
     .then(r => r.json())
-    .then(data => console.log(data));
+    .then(data => {
+      const markup = data.hits
+        .map(
+          item => `<div class="photo-card">
+          <img src="${item.previewURL}" alt="" loading="lazy" />
+          <div class="info">
+            <p class="info-item">
+              <b>${item.likes}</b>
+            </p>
+            <p class="info-item">
+              <b>${item.views}</b>
+            </p>
+            <p class="info-item">
+              <b>${item.comments}</b>
+            </p>
+            <p class="info-item">
+              <b>${item.downloads}</b>
+            </p>
+          </div>
+        </div>`
+        )
+        .join('');
+
+      getEl('.gallery').innerHTML += markup;
+    });
 }
-getData();
+
+const getEl = x => document.querySelector(x);
 
 inputRef.addEventListener('input', onInputChange);
 searchBtnRef.addEventListener('click', toMarkup);
 
 function onInputChange(e) {
-  console.log(e.target.value);
+  const InputValue = e.target.value;
+  console.log(InputValue);
 }
 
-function toMarkup() {}
+function toMarkup(e) {
+  e.preventDefault();
+
+  getData();
+}
+
+// .then(data => {
+//       const markup = data.hits
+//         .map(
+//           item => `<div class="photo-card">
+//           <img src="${data.pageURL}" alt="" loading="lazy" />
+//           <div class="info">
+//             <p class="info-item">
+//               <b>${data.likes}</b>
+//             </p>
+//             <p class="info-item">
+//               <b>${data.views}</b>
+//             </p>
+//             <p class="info-item">
+//               <b>${data.comments}</b>
+//             </p>
+//             <p class="info-item">
+//               <b>${data.downloads}</b>
+//             </p>
+//           </div>
+//         </div>`
+//         )
+//         .join('');
+//       getEl('.gallery').innerHTML += markup;
+//     });
