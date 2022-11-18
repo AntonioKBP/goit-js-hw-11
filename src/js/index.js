f8937a383b6148db0da77179b712e05047c2da65
Anton Kovsh <kovsh_anton@ukr.net>
2022-11-18 15:06:15 +0200

    upd

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,6 @@
-console.log('hello 11');
+const inputRef = document.querySelector('.search-form');
+const searchBtnRef = document.querySelector('button[type="submit"]');
+console.log(inputRef);
+console.log(searchBtnRef);
+
+// https://pixabay.com/api/?key=31349139-c34332f5cc1455d1f889740ec&q=yellow+flowers&image_type=photo
